d70734d419e6e9a14889a3360f13c7602936172c
Jarostaz <24418207+Jarostaz@users.noreply.github.com>
2026-01-04 05:13:20 +0100

    Title

---
+++ app/courses/cursor-mastery/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "The Replacer | Advanced Cursor Mastery | 200x.dev",
+};
+
+export default function CursorMasteryLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
+
